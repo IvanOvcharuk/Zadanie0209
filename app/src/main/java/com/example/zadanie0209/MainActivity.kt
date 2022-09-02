288b949b8534e9f65922b91615f93b5c2341458c
Hans_Panzer_2137_ <74077594+HansPanzer2137@users.noreply.github.com>
2022-09-02 16:40:51 +0200

    Gotowe zadanie

--- app/src/main/java/com/example/zadanie0209/MainActivity.kt
+++ app/src/main/java/com/example/zadanie0209/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.zadanie0209
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var obrazek = findViewById<ImageView>(R.id.obraz);
+        var textView = findViewById<TextView>(R.id.textView);
+        var button = findViewById<Button>(R.id.button);
+        findViewById<Button>(R.id.button).setOnClickListener {
+            if (obrazek.isVisible == true) {
+                button.text = "POKAZ"
+                textView.text = "Kot";
+                obrazek.isVisible = false;
+            }
+            else {
+                button.text = "UKRYJ"
+                textView.text = "Kot";
+                obrazek.isVisible = true;
+            }
+        }
+        }
     }
-}
